1608972ec2b53a785e5dbb0a27683518a9daf304
GGN_2015 <630230141@qq.com>
2023-10-21 01:35:39 +0800

    show-hide

--- script/controll.js
+++ script/controll.js
@@ -1,5 +1,30 @@
-function mylog(message) {
-    console.log("controll.js: " + message); // 输出日志
+import {
+    show_treble_clef, 
+    hide_treble_clef, 
+    show_bass_clef, 
+    hide_bass_clef
+} from "./show_hide.js";
+
+function get_timesyamp() {
+    var currentTime = new Date();
+    var hours       = currentTime.getHours()  .toString().padStart(2, "0");
+    var minutes     = currentTime.getMinutes().toString().padStart(2, "0");
+    var seconds     = currentTime.getSeconds().toString().padStart(2, "0");
+    return hours + ":" + minutes + ":" + seconds
 }
 
-mylog("online");
+function mylog(message) {
+    console.log("[" + get_timesyamp() + "] controll.js: " + message); // 输出日志
+}
+
+mylog("脚本可用");
+
+function mytest() {
+    show_treble_clef(); // 显示高音谱号
+    hide_bass_clef();   // 隐藏低音谱号
+}
+
+$(document).ready(function() { // 在页面加载完成后执行的代码
+    mylog("页面加载完成");
+    mytest();
+});
---
+++ script/show_hide.js
@@ -0,0 +1,15 @@
+export function show_treble_clef() { // 显示高音谱号
+    $("#img_TrebleClef").css("visibility", "visible");
+}
+
+export function hide_treble_clef() { // 隐藏高音谱号
+    $("#img_TrebleClef").css("visibility", "hidden");
+}
+
+export function show_bass_clef() { // 显示低音谱号
+    $("#img_BassClef").css("visibility", "visible");
+}
+
+export function hide_bass_clef() { // 隐藏低音谱号
+    $("#img_BassClef").css("visibility", "hidden");
+}
